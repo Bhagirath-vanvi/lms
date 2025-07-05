b16d86951a1a3abe8d109c12f8875e2cc44f727b
Bhagirath-vanvi <bhagirathvanvi07@gmail.com>
2025-07-05 11:36:36 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,14 @@ import paymentRoutes from "./routes/payment.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://lms-chi-opal-21.vercel.app",
+    credentials: true,
+  })
+);
+
 
 // Body parsing middleware
 app.use(express.json({ limit: "10mb" }));
